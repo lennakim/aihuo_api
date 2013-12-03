87e1ce1cf7fe8fc9c4e8557c53097fe815ef9dca
Victor Wang <wjp2013@gmail.com>
2013-12-03 13:00:46 +0800

    Fix device info set tag bug
    * Don’t set tag if device is nil

--- app/models/device_info.rb
+++ app/models/device_info.rb
@@ -15,17 +15,22 @@ class DeviceInfo < ActiveRecord::Base
   # class methods .............................................................
   # public instance methods ...................................................
   def set_date_tag
-    date_tag = device.try(:created_at).strftime("%y%m%d")
+    date_tag = device.try(:created_at).strftime("%y%m%d") if device
     update_attribute(:tag_list, date_tag) if date_tag
   end
 
   def send_sales_promotion_msg
     # 设备没有百度用户信息，不发送通知
     return if baidu_user_id.blank?
+
     # 设备注册时间在3天前，不发送通知
-    return if self.try(:device).try(:created_at) < 2.days.ago(Date.today)
     # 设备注册时间在3天内(前天，昨天，今天)，则发送0元购通知
-    Notification.send_sales_promotion_msg(device_id)
+    # TODO:
+    # *Date.today* maybe cached in production mode, so this line alwasy
+    #   return same value. need to test and fix it later.
+    if device && device.try(:created_at) > 2.days.ago(Date.today)
+      Notification.send_sales_promotion_msg(device_id)
+    end
   end
   # protected instance methods ................................................
   # private instance methods ..................................................
