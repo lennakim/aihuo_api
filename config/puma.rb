14b6b87cd069cf3bc9652f721a65859f7c84f70f
Victor Wang <wjp2013@gmail.com>
2014-02-20 15:32:16 +0800

    Chnage puma log and pid in different folder.

--- config/puma.rb
+++ config/puma.rb
@@ -7,13 +7,15 @@ workers 2
 threads 8, 32
 
 wd = File.expand_path('../../', __FILE__)
-tmp_path = File.join(wd, 'log')
+log_path = File.join(wd, 'log')
+tmp_path = File.join(wd, 'tmp')
+Dir.mkdir(log_path) unless File.exist?(log_path)
 Dir.mkdir(tmp_path) unless File.exist?(tmp_path)
 
-bind  "unix:///var/run/adultshop_api.sock"
-pidfile File.join(tmp_path, 'api_puma.pid')
-state_path File.join(tmp_path, 'api_puma.state')
-stdout_redirect File.join(tmp_path, 'api_puma.out.log'), File.join(tmp_path, 'api_puma.err.log'), true
+bind  "unix:///var/run/api.aihuo360.com.sock"
+pidfile File.join(tmp_path, 'puma.pid')
+state_path File.join(tmp_path, 'puma.state')
+stdout_redirect File.join(log_path, 'puma.out.log'), File.join(log_path, 'puma.err.log'), true
 
 preload_app! #utilizing copy-on-write
 activate_control_app
