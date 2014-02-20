9d22165ea973f41f027691b6288c86dd3a164da2
Victor Wang <wjp2013@gmail.com>
2014-02-20 15:25:49 +0800

    Change aliyun OSS to use production bucket.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 require 'carrierwave'
 CarrierWave.configure do |config|
-  # config.aliyun_bucket = "blsm-public"
-  # config.aliyun_host = "image.yepcolor.com"
+  config.aliyun_bucket = "blsm-public"
+  config.aliyun_host = "image.yepcolor.com"
   # config.aliyun_bucket = "test8811609"
-  config.aliyun_host = "test8811609.oss.aliyuncs.com"
+  # config.aliyun_host = "test8811609.oss.aliyuncs.com"
 end
