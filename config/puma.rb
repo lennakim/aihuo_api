7c2b3590d2cab40d21df8ff3e1b032fe20c6d20d
Victor Wang <wjp2013@gmail.com>
2014-02-20 18:47:03 +0800

    change puma config and rvm config.

--- config/puma.rb
+++ config/puma.rb
@@ -1,21 +1,35 @@
 require 'puma'
 #!/usr/bin/env puma
-environment "production"
+
+# Start Puma with next command:
+# bundle exec puma -e production -C ./config/puma.rb
+application_path = '/var/www/api.aihuo360.com/current'
+
+# The directory to operate out of.
+directory application_path
+
+# Set the environment in which the rack's app will run.
+environment 'production'
+
+# Daemonize the server into the background. Highly suggest that
+# this be combined with `pidfile` and `stdout_redirect`.
 daemonize true
 
 workers 2
 threads 8, 32
 
-wd = File.expand_path('../../', __FILE__)
-log_path = File.join(wd, 'log')
-tmp_path = File.join(wd, 'tmp')
-Dir.mkdir(log_path) unless File.exist?(log_path)
-Dir.mkdir(tmp_path) unless File.exist?(tmp_path)
+# Store the pid of the server in the file at `path`.
+pidfile "#{application_path}/tmp/pids/puma.pid"
 
-bind  "unix:///var/run/api.aihuo360.com.sock"
-pidfile File.join(tmp_path, 'puma.pid')
-state_path File.join(tmp_path, 'puma.state')
-stdout_redirect File.join(log_path, 'puma.out.log'), File.join(log_path, 'puma.err.log'), true
+# Use `path` as the file to store the server info state. This is
+# used by `pumactl` to query and control the server.
+state_path "#{application_path}/tmp/pids/puma.state"
+
+# Redirect STDOUT and STDERR to files specified.
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+
+# Bind the server.
+bind "unix://#{application_path}/tmp/sockets/puma.sock"
 
 preload_app! #utilizing copy-on-write
-activate_control_app
+activate_control_app "unix://#{application_path}/tmp/sockets/puma.sock"
