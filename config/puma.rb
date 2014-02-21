0d34c28301cc6920bb5d6eec5237b954959d5bcb
Victor Wang <wjp2013@gmail.com>
2014-02-21 10:59:13 +0800

    Reset puma config file to make production works.

--- config/puma.rb
+++ config/puma.rb
@@ -1,35 +1,19 @@
 require 'puma'
 #!/usr/bin/env puma
-
-# Start Puma with next command:
-# bundle exec puma -e production -C ./config/puma.rb
-application_path = '/var/www/api.aihuo360.com/current'
-
-# The directory to operate out of.
-directory application_path
-
-# Set the environment in which the rack's app will run.
-environment 'production'
-
-# Daemonize the server into the background. Highly suggest that
-# this be combined with `pidfile` and `stdout_redirect`.
+environment "production"
 daemonize true
 
 workers 2
 threads 8, 32
 
-# Store the pid of the server in the file at `path`.
-pidfile "#{application_path}/tmp/pids/puma.pid"
+wd = File.expand_path('../../', __FILE__)
+tmp_path = File.join(wd, 'log')
+Dir.mkdir(tmp_path) unless File.exist?(tmp_path)
 
-# Use `path` as the file to store the server info state. This is
-# used by `pumactl` to query and control the server.
-state_path "#{application_path}/tmp/pids/puma.state"
-
-# Redirect STDOUT and STDERR to files specified.
-stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
-
-# Bind the server.
-bind "unix://#{application_path}/tmp/sockets/puma.sock"
+bind  "unix:///var/run/adultshop_api.sock"
+pidfile File.join(tmp_path, 'api_puma.pid')
+state_path File.join(tmp_path, 'api_puma.state')
+stdout_redirect File.join(tmp_path, 'api_puma.out.log'), File.join(tmp_path, 'api_puma.err.log'), true
 
 preload_app! #utilizing copy-on-write
-activate_control_app "unix://#{application_path}/tmp/sockets/puma.sock"
+activate_control_app
