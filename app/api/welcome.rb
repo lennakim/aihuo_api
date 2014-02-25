e7c950c356f8c7484c589a44ceebf883d61c9b36
Victor Wang <wjp2013@gmail.com>
2014-02-25 11:18:17 +0800

    Return banner of the current application in home api.
    * add resource model.

--- app/api/welcome.rb
+++ app/api/welcome.rb
@@ -4,7 +4,8 @@ class Welcome < Grape::API
     optional :date, type: Integer, desc: "Device created date."
   end
   get :home, jbuilder: 'welcome/home' do
-    @banners = Article.banner.without_gifts
+    current_application
+    @banners = @application.articles.banner
     @tags = Tag.where(id: Tag::CATEGORIES)
     cache(key: [:v2, :home, @banners.last], expires_in: 2.days) do
       @banners
