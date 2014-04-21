66a2769c2fa1fc5b4338680c82653920b243ba8f
Victor Wang <wjp2013@gmail.com>
2014-04-21 20:11:00 +0800

    send private message need member password

--- lib/apis/private_messages.rb
+++ lib/apis/private_messages.rb
@@ -9,7 +9,7 @@ class PrivateMessages < Grape::API
       sender_id =
         EncryptedId.decrypt(
           Member.encrypted_id_key,
-          params[:private_message][:sender_id]
+          params[:member_id]
         )
       params[:private_message][:receiver_id] = receiver_id
       params[:private_message][:sender_id] = sender_id
@@ -18,30 +18,40 @@ class PrivateMessages < Grape::API
       declared(params, include_missing: false)[:private_message]
     end
 
-    def receiver_param
+    def receiver_id
       EncryptedId.decrypt(Member.encrypted_id_key, params[:member_id])
     end
-  end
 
+    def authenticate?
+      member = Member.find params[:member_id]
+      member && member.authenticate?(params[:password])
+    end
+  end
 
   resources 'private_messages' do
 
     desc "Listing private message for the user."
     params do
       requires :member_id, type: String, desc: "Member ID."
+      requires :password, type: String, desc: "Member Password."
       requires :filter, type: Symbol, values: [:inbox, :spam], default: :inbox, desc: "Message Type."
       optional :page, type: Integer, desc: "Page number."
       optional :per_page, type: Integer, default: 10, desc: "Per page value."
     end
 
     get '/', jbuilder: 'private_messages/messages' do
-        messages = case params[:filter]
+      messages =
+        case params[:filter]
         when :inbox
           PrivateMessage
         when :spam
           PrivateMessage.unscoped.spam
         end
-      @private_messages = paginate(messages.by_receiver(receiver_param))
+      if authenticate?
+        @private_messages = paginate(messages.by_receiver(receiver_id))
+      else
+        error! "Access Denied", 401
+      end
     end
 
     desc "Create a private message."
@@ -50,20 +60,20 @@ class PrivateMessages < Grape::API
       group :private_message, type: Hash do
         requires :body, type: String, desc: "Private message content."
         requires :receiver_id, type: String, desc: "Receiver id."
-        requires :sender_id, type: String, desc: "Sender id."
+        optional :sender_id, type: String, desc: "Sender id."
       end
+      requires :member_id, type: String, desc: "Member ID."
+      requires :password, type: String, desc: "Member Password."
     end
 
     post "/", jbuilder: 'private_messages/message' do
-      if sign_approval?
+      if sign_approval? && authenticate?
         @private_message = PrivateMessage.new(private_message_params)
         @private_message.save
       else
         error! "Access Denied", 401
       end
     end
-
-
 
   end
 end
