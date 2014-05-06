cf140438c53312fcb62919eb3e4a58763766c66b
Victor Wang <wjp2013@gmail.com>
2014-05-06 11:33:08 +0800

    change forum validations on carete action

--- lib/concerns/forum_validations.rb
+++ lib/concerns/forum_validations.rb
@@ -4,13 +4,13 @@ module ForumValidations
   included do
     validates_presence_of :body
 
-    validate do
+    validate on: :create do
       error_msg = "您已被禁言，有问题请去版务区发帖申诉"
       errors.add(:device_id, error_msg) if Blacklist.available.find_by(device_id: device_id, node_id: 0)
       errors.add(:device_id, error_msg) if Blacklist.find_by(device_id: device_id, node_id: node_id)
     end
 
-    # validate do
+    # validate on: :create do
     #   error_msg = "内容包含敏感词，有问题请去版务区发帖申诉"
     #   ban_words = (Setting.find_by_name("ban_words").value || "").split("\n")
     #   ban_words.each do |word|
