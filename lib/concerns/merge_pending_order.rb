158238a1e686c427570320a390e0836d05dc6ea5
Victor Wang <wjp2013@gmail.com>
2014-05-08 16:08:41 +0800

    call update_payment_state after calculate_item_total when order merge other pending orders

--- lib/concerns/merge_pending_order.rb
+++ lib/concerns/merge_pending_order.rb
@@ -20,13 +20,14 @@ module MergePendingOrder
   def calculate_payment_total_with_pending_orders(orders)
     orders_payment_total = orders.inject(self.payment_total) { |sum, o| sum + o.payment_total }
     self.update_column(:payment_total, orders_payment_total)
-    update_payment_state
   end
 
   # 汇总已经订单产品金额, 创建，合并订单时调用此方法
   def calculate_item_total
     item_total = line_items.inject(0){ |sum, item| sum + item.sale_price * item.quantity }
     update({item_total: item_total})
+    # 更新支付状态
+    update_payment_state
     # 重新计算运费, 包邮逻辑放在其他 concerns 中
     calculate_shipping_charge
   end
