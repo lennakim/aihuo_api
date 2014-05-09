227338c6bcb6edd1e1b4dfc963abecd3a4bba092
Victor Wang <wjp2013@gmail.com>
2014-05-09 14:36:09 +0800

    quick fix

--- config/puma.rb
+++ config/puma.rb
@@ -54,7 +54,7 @@ on_worker_boot do
   ActiveRecord::Base.connection_pool.disconnect!
 
   ActiveSupport.on_load(:active_record) do
-    config = YAML.load(ERB.new(File.read('config/database.yml')).result)[environment]
+    config = YAML.load(ERB.new(File.read('config/database.yml')).result)[ENV['RACK_ENV']]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL']      || ENV['MAX_THREADS'] || 5
     ActiveRecord::Base.establish_connection(config)
