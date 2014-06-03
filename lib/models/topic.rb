21b9c54c563f3f0dbe602e53d7ae720392139e2e
Victor Wang <wjp2013@gmail.com>
2014-06-03 11:49:48 +0800

    don't show topic in lasted tab if replis more than 50
    http://blsm.5upm.com/task-view-124.html

--- lib/models/topic.rb
+++ lib/models/topic.rb
@@ -20,7 +20,9 @@ class Topic < ActiveRecord::Base
   scope :approved, -> { where(approved: true) }
   scope :by_device, ->(device_id) { where(device_id: device_id) }
   scope :popular, -> { where("replies_count >= 50") }
-  scope :lasted, -> { where(best: false).reorder("top DESC, updated_at DESC") }
+  scope :lasted, -> {
+    where(best: false).where("replies_count <= 50") .reorder("top DESC, updated_at DESC")
+  }
   scope :excellent, -> { where(best: true).reorder("top DESC, updated_at DESC") }
   scope :checking, -> { where(approved: false) }
   # additional config (i.e. accepts_nested_attribute_for etc...) ..............
