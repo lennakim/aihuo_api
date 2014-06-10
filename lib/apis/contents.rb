064f5b8198615daa2d8d066755031b4794285ab2
Victor Wang <wjp2013@gmail.com>
2014-06-10 19:52:00 +0800

    add dislike content api

--- lib/apis/contents.rb
+++ lib/apis/contents.rb
@@ -10,9 +10,9 @@ class Contents < Grape::API
       optional :per_page, type: Integer, default: 10, desc: "Per page value."
     end
     get "/", jbuilder: 'contents/contents' do
-      cache(key: [:v2, :contents, params[:page], params[:per_page]], expires_in: 4.hours) do
+      # cache(key: [:v2, :contents, params[:page], params[:per_page]], expires_in: 4.hours) do
         @contents = paginate(Content)
-      end
+      # end
     end
 
 
@@ -28,9 +28,9 @@ class Contents < Grape::API
 
       desc "Return a content."
       get "/", jbuilder: 'contents/content'  do
-        cache(key: [:v2, :content, @content], expires_in: 1.days) do
+        # cache(key: [:v2, :content, @content], expires_in: 1.days) do
           @content
-        end
+        # end
       end
 
       desc "Like a content."
@@ -39,11 +39,60 @@ class Contents < Grape::API
         status 202
       end
 
+      desc 'Unlike a content.'
+      put :dislike, jbuilder: 'contents/content' do
+        @content.disliked
+        status 202
+      end
+
       desc "Repost a content"
       put :forward, jbuilder: 'contents/content' do
         @content.forward
         status 202
       end
+
+      resources 'replies' do
+        desc "Return a listing of replies for a content."
+        params do
+          optional :page, type: Integer, default: 1, desc: "Page number."
+          optional :per_page, type: Integer, default: 10, desc: "Per page value."
+        end
+        get "/", jbuilder: 'replies/replies' do
+          @replies = paginate(@content.replies)
+        end
+
+        desc "Create a reply to the content."
+        params do
+          requires :body, type: String, desc: "Reply content."
+          requires :nickname, type: String, desc: "User nickname."
+          requires :device_id, type: String, desc: "Deivce ID."
+          requires :sign, type: String, desc: "sign value."
+          optional :member, type: Hash do
+            requires :id, type: String, desc: "Member ID."
+            requires :password, type: String, desc: "Member password."
+          end
+        end
+        post "/", jbuilder: 'replies/reply' do
+          if sign_approval?
+            @reply = @content.replies.new({
+                       body: params[:body],
+                       nickname: params[:nickname],
+                       device_id: params[:device_id]
+                     })
+            if params[:member]
+              @reply.relate_to_member_with_authenticate(
+                params[:member][:id],
+                params[:member][:password]
+              )
+            end
+            status 422 unless @reply.save
+          else
+            error! "Access Denied", 401
+          end
+        end
+      end
+
+
     end
 
 
--- lib/views/contents/_content.jbuilder
+++ lib/views/contents/_content.jbuilder
@@ -1,2 +1,2 @@
 json.id content.to_param
-json.(content, :user_name, :user_avatar, :text, :image, :width, :height, :likes_count, :forward_count, :replies_count, :top, :created_at)
+json.(content, :user_name, :user_avatar, :text, :image, :width, :height, :likes_count, :unlikes_count, :forward_count, :replies_count, :top, :created_at)
