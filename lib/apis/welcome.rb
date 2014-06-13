fcee3f2a54b931d8756d8d516a8d4511281489e4
Victor Wang <wjp2013@gmail.com>
2014-06-13 10:52:40 +0800

    home data form database by different channel

--- lib/apis/welcome.rb
+++ lib/apis/welcome.rb
@@ -19,50 +19,19 @@ class Welcome < Grape::API
   end
   get :home, jbuilder: 'welcome/home' do
     current_application
+    page_for_360 = Homepage.find_by(label: "首趣啪啪360")
+    page_for_authority = Homepage.find_by(label: "首趣啪啪官方")
+    page_for_skin = Homepage.find_by(label: "首趣啪啪皮肤")
 
     case params[:filter]
     when :healthy
       @banners = Article.healthy.limit(2)
-      @submenus = [
-        {id: "", type: 'Tag', title: '最新产品', name: "最新产品", image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/1/1-01.png'},
-        {id: "", type: 'Tag', title: '小编首推', name: "小编首推", image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/1/1-02.png'},
-        {id: "", type: 'Tag', title: '火爆套套', name: "火爆套套", image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/1/1-03.png'}
-      ]
+      @submenus = page_for_skin.contents.submenus
       @categories = []
       @sections = [
-        {
-          name: "热销区",
-          objects: [
-            {id: "", type: 'Tag', title: '火爆热销', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/huodingyi-1.png'},
-            {id: "b0fb128360d9eb2171819a741f7d0eda", type: 'Product', title: '360随身WIFI', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-01.png'},
-            {id: "d83c8ea92c4790886bf52bc99bd225b1", type: 'Product', title: '冰希黎香水', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-02.png'},
-            {id: "737b636c96a09d2d9afad194489f46ff", type: 'Product', title: '短袖睡衣', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-03.png'},
-            {id: "1e999e791e9d22690834f36f1b8424c9", type: 'Product', title: '压床娃娃', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-04.png'},
-            {id: "5a35dc7f93a32a255aec912682d9d810", type: 'Product', title: '调情香氛', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-05.png'}
-          ]
-        },
-        {
-          name: "新品区",
-          objects: [
-            {id: "41aa3fac21a9f0c681878d8c337c3d1b", type: 'Article', title: '', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/huodong-2.png'},
-            {id: "97bf5112af787c1ede0ae5286aaeef59", type: 'Product', title: '头戴耳机', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-06.png'},
-            {id: "2ae1e396ac0844323c08a0839c5e123a", type: 'Product', title: '丹姿水密码', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-07.png'},
-            {id: "1a338177155b847cf46bd6dd884f38cf", type: 'Product', title: '舔食润滑液', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-08.png'},
-            {id: "77fe08c4207a11fee2aa05cc6baa89c4", type: 'Product', title: '金鱼树脂', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-09.png'},
-            {id: "38f213dc99a2e4aa1e03b78e498d412a", type: 'Product', title: '笔筒高档', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-10.png'}
-          ]
-        },
-        {
-          name: "主题馆",
-          objects: [
-            {id: "", type: 'Tag', title: '日常战备', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/huodong-3.jpg'},
-            {id: "afd3988ca620a9c5d2e8cf90b8b40b16", type: 'Product', title: '糖果安全套', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-11.png'},
-            {id: "cff809c998e8b2f83482105da60877ff", type: 'Product', title: '活力螺纹', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-12.png'},
-            {id: "3931f4abe7c946249eba777e981f2f1e", type: 'Product', title: '薄如羽翼', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-13.png'},
-            {id: "facc69582169e4d0359a60963b02d646", type: 'Product', title: '精品文胸', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-14.png'},
-            {id: "5b3aed411b575776f855a1dc5aefb7fd", type: 'Product', title: '私处洗液', image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/majia/2/2-15.png'}
-          ]
-        }
+        page_for_skin.contents.sections(1),
+        page_for_skin.contents.sections(2),
+        page_for_skin.contents.sections(3),
       ]
       @brands = []
     when :all
@@ -77,21 +46,17 @@ class Welcome < Grape::API
         end
       # @tags = Tag.where(id: Tag::CATEGORIES)
       if params[:ref] && params[:ref] == "360"
-        @submenus = [
-          {id: "", type: 'Tag', title: '新品特价', name: "新品特价", image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/home/1/1-01.png'},
-          {id: "3f9d7b0f3e42047bae820ab941d75a23", type: 'Article', title: '360特权专享',image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/home/1/1-04.png'},
-          {id: "", type: 'Tag', title: '火爆套套', name: "火爆套套", image: 'http://blsm-public.oss.aliyuncs.com/images/20140509/home/1/1-03.png'}
-        ]
+        @submenus = page_for_360.contents
       else
-        @submenus = HomeContent.submenus
+        @submenus = page_for_authority.contents.submenus
       end
-      @categories = HomeContent.categories
+      @categories = page_for_authority.contents.categories
       @sections = [
-        HomeContent.sections(1),
-        HomeContent.sections(2),
-        HomeContent.sections(3)
+        page_for_authority.contents.sections(1),
+        page_for_authority.contents.sections(2),
+        page_for_authority.contents.sections(3),
       ]
-      @brands = HomeContent.brands
+      @brands = page_for_authority.contents.brands
     end
 
 
