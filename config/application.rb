8e8591b6a3a7fda0b9b78a3bca72bf841ebfc396
Victor Wang <wjp2013@gmail.com>
2014-06-24 21:38:40 +0800

    change timezone to beijing

--- config/application.rb
+++ config/application.rb
@@ -20,13 +20,15 @@ module ShouquApi
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Beijing'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
+
+    config.active_record.default_timezone = :local
 
     config.cache_store = :file_store, "tmp/cache"
   end
