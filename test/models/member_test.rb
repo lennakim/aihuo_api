384ca02d22eb30c27f9b5136989f7952aac19bd0
Victor Wang <wjp2013@gmail.com>
2014-08-18 17:07:58 +0800

    修改 member 的验证方法，符合 ruby 推荐语法，并测试覆盖

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -10,6 +10,16 @@ class MembersTest < ActiveSupport::TestCase
     assert @member_1.valid?
   end
 
+  def test_member_verified_with_increase_coin
+    @member_1.verified!
+    assert_equal 20, @member_1.coin_total
+  end
+
+  def test_member_verified_without_increase_coin
+    @member_1.verified!(need_increase_coin: false)
+    assert_equal 5, @member_1.coin_total
+  end
+
   # def test_send_sms
   #   result = @member_1.send_captcha("13641374170")
   #   assert result[:success]
