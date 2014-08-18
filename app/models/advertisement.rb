12d9146ede7fef00fe232efabbf38dca4c4e74e8
Victor Wang <wjp2013@gmail.com>
2014-08-18 12:35:42 +0800

    返回的广告列表根据安装量设定返回条件

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -3,11 +3,18 @@ class Advertisement < ActiveRecord::Base
   # includes ..................................................................
   include CarrierWave
   # relationships .............................................................
+  has_many :adv_statistics, foreign_key: "adv_content_id"
   # validations ...............................................................
   # callbacks .................................................................
   # scopes ....................................................................
-  default_scope {
-    where(activity: true).where("today_view_count < actual_view_count")
+  # default_scope {
+  #   where(activity: true).where("today_view_count < actual_view_count")
+  #     .order("updated_at DESC")
+  # }
+  scope :available, -> {
+    joins(:adv_statistics).merge(AdvStatistic.today)
+      .where(activity: true)
+      .where("adv_statistics.install_count < adv_contents.plan_view_count")
       .order("updated_at DESC")
   }
   # additional config (i.e. accepts_nested_attribute_for etc...) ..............
