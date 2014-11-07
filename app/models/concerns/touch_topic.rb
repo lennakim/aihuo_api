f73a00fd4f4bc9212e5c3d022a1e81dccb0c7978
Victor Wang <wjp2013@gmail.com>
2014-11-07 15:45:48 +0800

    a bit of refactor

--- app/models/concerns/touch_topic.rb
+++ app/models/concerns/touch_topic.rb
@@ -8,17 +8,15 @@ module TouchTopic
   protected
   # 帖子创建时间在5天内的，更新 updated_at
   def save_and_touch_topic_updated_at
-    case self
-    when Reply
-      if topic && topic.created_at > 5.days.ago(Date.today)
-        topic.updated_at = Time.now
-        topic.save(validate: false)
+    object =
+      case self
+      when Reply then topic
+      when Topic then self
       end
-    when Topic
-      if created_at > 5.days.ago(Date.today)
-        self.updated_at = Time.now
-        save(validate: false)
-      end
+
+    if object.created_at > 5.days.ago(Date.today)
+      object.updated_at = Time.now
+      object.save(validate: false)
     end
   end
 
