7a2adcd2ff561b79a9ae652f76c04f0e7b262dd1
Victor Wang <wjp2013@gmail.com>
2014-12-22 11:27:21 +0800

    add  a bit of test

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -10,4 +10,20 @@ class SettingTest < ActiveSupport::TestCase
     setting = Setting.find_by_name(:online_shipping_fee)
     assert_equal "24.0", setting.value
   end
+
+  def test_fetch_by_key
+    key = "private_message_send_for_register_member"
+    setting = Setting.fetch_by_key key
+    assert_equal "asndfansdfo", setting
+  end
+
+  def test_invitation_content
+    setting = Setting.invitation_content
+    assert_equal "asndfansdfo", setting
+  end
+
+  def test_invitation_sender
+    setting = Setting.invitation_sender
+    assert_equal "2", setting
+  end
 end
