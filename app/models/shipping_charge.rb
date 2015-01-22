bc5fde60b92398ae67ebc3679d51adbbe0476f50
Victor Wang <wjp2013@gmail.com>
2015-01-22 17:14:14 +0800

    a bit of refactor

--- app/models/shipping_charge.rb
+++ app/models/shipping_charge.rb
@@ -1,28 +1,3 @@
-module ShippingChargeMethod
-  class << self
-    def default_shipping_charge; [12, 12]; end
-    alias_method :shipping_charge, :default_shipping_charge
-  end
-
-  def shipping_charge
-    get_shippint_charge(superior_region)
-  end
-
-  private
-
-  def superior_region
-    self
-  end
-
-  def get_shippint_charge(obj = nil)
-    if cash_on_delivery == 0 || pay_online == 0
-      obj.nil? ? ShippingChargeMethod.default_shipping_charge : obj.shipping_charge
-    else
-      [cash_on_delivery, pay_online]
-    end
-  end
-end
-
 class Province < ActiveRecord::Base
   has_many :cities, dependent: :destroy
   include ShippingChargeMethod
@@ -59,7 +34,7 @@ class ShippingCharge
     district = city.districts.find_by_name(district_name) if city
 
     cash_on_delivery, pay_online =
-      [district, city, province, ShippingChargeMethod].select(&:present?).first.shipping_charge
+      [district, city, province, self].select(&:present?).first.shipping_charge
     Array.new(1, ShippingCharge.new(cash_on_delivery, pay_online))
   end
 end
